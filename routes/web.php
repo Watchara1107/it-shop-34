51f03df5feb72462d08289f4412de6affd68e2b9
Watchara Ketkaew <wk996110@gmail.com>
2022-11-14 10:12:15 +0700

    update frontend

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[App\Http\Controllers\ProductController::class, 'index']);
 
 Route::get('/about',function(){
     return "หน้าเกี่ยวกับเรา";
